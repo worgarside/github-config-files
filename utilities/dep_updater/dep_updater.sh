0b64ce30defa62a5b49379148bf0505ffa394061
Will Garside <worgarside@gmail.com>
2022-08-01 21:22:47 +0100

    Update to account for no files

--- utilities/dep_updater/dep_updater.sh
+++ utilities/dep_updater/dep_updater.sh
@@ -8,13 +8,19 @@ branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 # tr ...  : replaces the spaces in the output of sed with newlines so we can iterate through
 changed_files=$(git diff --stat "${branch}" origin/"${branch}" | sed -n -e 's/\([A-Za-z0-9_\-\.]*\) \| .*$/\1/p' | tr " " "\n")
 
-pipfiles_only=true
-# iterate through all changed files
-for file in $changed_files
-do
-    # if the file doesn't match the regex then set the flag to false
-    ! [[ "$file" =~  ^Pipfile(.lock)?$ ]] && pipfiles_only=false
-done
+if [[ $changed_files == '' ]]; then
+  # no files
+  pipfiles_only=false
+else
+  # some files
+  pipfiles_only=true
+  # iterate through all changed files
+  for file in $changed_files
+  do
+      # if the file doesn't match the regex then set the flag to false
+      ! [[ "$file" =~  ^Pipfile(.lock)?$ ]] && pipfiles_only=false
+  done
+fi
 
 #if only pipfiles have changed, then pull and install
 if $pipfiles_only; then
