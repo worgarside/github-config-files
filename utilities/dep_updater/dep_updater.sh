e9b6b74dae4bf0dfa32930d702dadbf02e8f15d9
Will Garside <worgarside@gmail.com>
2022-08-01 21:18:20 +0100

    Add dep updater

---
+++ utilities/dep_updater/dep_updater.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# gets current branch
+branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+
+# git ... : lists all files changed on the remote
+# sed ... : formats output to get just file names (based on strings followed by a pipe)
+# tr ...  : replaces the spaces in the output of sed with newlines so we can iterate through
+changed_files=$(git diff --stat "${branch}" origin/"${branch}" | sed -n -e 's/\([A-Za-z0-9_\-\.]*\) \| .*$/\1/p' | tr " " "\n")
+
+pipfiles_only=true
+# iterate through all changed files
+for file in $changed_files
+do
+    # if the file doesn't match the regex then set the flag to false
+    ! [[ "$file" =~  ^Pipfile(.lock)?$ ]] && pipfiles_only=false
+done
+
+#if only pipfiles have changed, then pull and install
+if $pipfiles_only; then
+  git pull
+  pipenv install --skip-lock
+fi
