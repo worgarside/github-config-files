7bb3ef4b0b0f965ebfb41acf894d6935668263ff
Will Garside <worgarside@gmail.com>
2023-11-18 19:48:27 +0000

    Update deployment rejector logging

--- utilities/reject_stale_deployments/reject_stale_deployments.py
+++ utilities/reject_stale_deployments/reject_stale_deployments.py
@@ -2,8 +2,9 @@
 from __future__ import annotations
 
 from functools import lru_cache
-from logging import getLogger
-from os import environ
+from logging import getLevelNamesMapping, getLogger
+from os import environ, getenv
+from pathlib import Path
 
 from httpx import Client
 
@@ -16,7 +17,23 @@ CLIENT = Client(
 )
 
 LOGGER = getLogger(__name__)
-LOGGER.setLevel("DEBUG")
+LOGGER.setLevel(environ["LOG_LEVEL"])
+
+
+def markdown_url(url: str, *, text: str | None = None) -> str:
+    """Return a Markdown link."""
+    if text is None:
+        text = url
+
+    return f"[{text}]({url})"
+
+
+@lru_cache(maxsize=1)
+def repo_name_markdown(repo_name: str, *, path: str | None = None) -> str:
+    """Return a Markdown link to the repo."""
+    path = "/" + path.strip("/") if path else ""
+
+    return markdown_url(f"https://github.com/{repo_name}{path}", text=repo_name)
 
 
 @lru_cache(maxsize=1)
@@ -25,6 +42,14 @@ def get_environments(repo_name: str) -> dict[str, str]:
     res = CLIENT.get(f"/repos/{repo_name}/environments")
 
     res.raise_for_status()
+
+    LOGGER.debug(
+        "Environments for %s are %s",
+        repo_name_markdown(repo_name),
+        ", ".join(
+            f"`{env['name']}` (`{env['id']}`)" for env in res.json()["environments"]
+        ),
+    )
 
     return {env["name"]: env["id"] for env in res.json()["environments"]}
 
@@ -67,6 +92,16 @@ def reject_stale_pending_deployments(repo_name: str) -> int:
             latest_env_deployment_sha,
         ) = get_deployment_shas(environment, repo_name)
 
+        LOGGER.debug(
+            "Latest deployment SHA for %s on `%s` is `%s`",
+            repo_name_markdown(repo_name),
+            markdown_url(
+                f"https://github.com/{repo_name}/deployments/{environment}",
+                text=environment,
+            ),
+            latest_env_deployment_sha,
+        )
+
         latest_deployment_shas.append(latest_env_deployment_sha)
 
     rejection_count = 0
@@ -107,14 +142,29 @@ def main() -> None:
         ):
             continue
 
-        rejected_deployments = reject_stale_pending_deployments(repo["full_name"])
-
-        LOGGER.info(
-            "Rejected %s stale pending deployments for %s",
-            rejected_deployments,
-            repo["full_name"],
-        )
+        try:
+            rejected_deployments = reject_stale_pending_deployments(repo["full_name"])
+        except Exception:  # pylint: disable=broad-exception-caught
+            LOGGER.exception(
+                "Failed to reject stale pending deployments for %s",
+                repo_name_markdown(repo["full_name"]),
+            )
+        else:
+            LOGGER.info(
+                "Rejected %s stale pending deployments for %s",
+                rejected_deployments,
+                repo_name_markdown(repo["full_name"]),
+            )
 
 
 if __name__ == "__main__":
+    if _step_summary_file := getenv("GITHUB_STEP_SUMMARY"):
+        from wg_utilities.loggers import add_file_handler
+
+        add_file_handler(
+            LOGGER,
+            logfile_path=Path(_step_summary_file),
+            level=getLevelNamesMapping()[environ["LOG_LEVEL"]],
+        )
+
     main()
